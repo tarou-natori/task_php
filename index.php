e517fb1387841dea3afd2c4872e5c4adc8ce12f6
名取裕太郎 <>
2023-04-08 10:44:39 +0900

    課題①-1完了

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>課題①-1</title>
+</head>
+<body>
+  <form action="" method="post">
+    <p>日本の首都は？</p>
+    <input type="text" name="answer">
+    <input type="submit" value="OK">
+  </form>
+  <?php
+  $answer = $_POST["answer"];
+  if ($answer == "東京") {
+    echo "正解";
+  }else{
+    echo "不正解";
+  }
+  ?>
+</body>
+</html>
